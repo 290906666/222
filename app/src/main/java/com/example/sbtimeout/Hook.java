eb2020c277864c57b674c1aee50d474d85790b73
一生何所求 <123272175+290906666@users.noreply.github.com>
2026-09-18 11:20:02 +0800

    Add files via upload

--- app/src/main/java/com/example/sbtimeout/Hook.java
+++ app/src/main/java/com/example/sbtimeout/Hook.java
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class Hook implements IXposedHookLoadPackage {
